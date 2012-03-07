2a8ba97ec01b636f30ca53afaa71da5fa4642b3e
조형규 <dreamboy@miraesoft.kr>
2012-03-07 17:39:48 +0900

    - taglib 의존성 추가. - viewResolver 설정 추

--- src/main/java/kr/miraesoft/promise/WebConfig.java
+++ src/main/java/kr/miraesoft/promise/WebConfig.java
@@ -1,10 +1,13 @@
 package kr.miraesoft.promise;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 
 @Configuration @EnableWebMvc @ComponentScan(basePackages = "kr.miraeosoft.promise") 
@@ -14,6 +17,14 @@ public class WebConfig extends WebMvcConfigurerAdapter{
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("**").addResourceLocations("/");
+	}
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
 	}
 
 }
